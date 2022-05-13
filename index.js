14f6faf98f0d37f51a5fc3dafcf26dfce1a89780
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-05-13 08:42:40 -0500

    updated questions to array

--- index.js
+++ index.js
@@ -2,105 +2,14 @@ document.body.style.backgroundImage = "url('./assets/krzyztopor-castle-1232338.j
  
 
 var myQuestions = [
-    {
-        question:  "Which film ranked first in domestic box office for 2019 ",
-        answers: {
-            a: 'Captain Marvel',
-            b: 'Star WArs: Episode IX - The Rise of Skywalker',
-            c: 'Toy Story 4',
-            d: 'Avengers: Endgame',
-        },
-        correctAnswer: 'd',
-    },
-    {
-        question: "Which Actress played Danielle De Barbarac in the film Ever After",
-        answers: {
-            a:  'Maggie Gyllenhall',
-            b:  'Drew Barrymore',
-            c:  'Scarlett Johansson',
-            d:  'Jennifer Connelly',
-        },
-        correctAnswer: b
-    },
-
-    {
-        question:  "What actor played the Joker in 2019",
-        answers: {
-            a: 'Heath Ledger ',
-            b: 'Joaquin Phoenix ',
-            c: 'Jared Leto ',
-            d: 'Jack Nicholson ',
-        },
-        correctAnswer: 'b'
-    },
-    {
-        question:  "What sequel made over 200,000,000 in 2019",
-        answers: {
-            a: 'Jumanji: The Next Level ',
-            b: 'It Chapeter Two ',
-            c: 'The Secret Life of Pets 2 ',
-            d: 'Zombieland: Double Tap ',
-        },
-        correctAnswer: 'b',
-    },
-    {
-        question:  "Which actor was the voice of Buzz Lightyear in Disney Pixar's Toy Story Franchise?",
-        answers: {
-            a: 'Timmothy Hutton',
-            b: 'Tom Hardy',
-            c: 'Tim Allen',
-            d: 'Tom Hanks',
-        },
-        correctAnswer: 'c'
-    },
-    {
-        question: "Which Actor did not star in 1994's Pulp Fiction",
-        answers: {
-            a: 'Bruce Willis ',
-            b: 'Christopher Walken',
-            c: 'Harvey Keitel ',
-            d: 'Patricia Arquette',
-        },
-        correctAnswer: 'd',
-    },
-    {
-        question:  "What did Hannibal Lecter eat with some fava beans and a nice Chianti?",
-        answers: {
-            a: 'Brain ',
-            b: 'Lung ',
-            c: 'Liver ',
-            d: 'Kidney ',
-        },
-        correctAnswer: 'c'
-    },
-    {
-        question:  "Which of the following did Tom Cruise NOT star in?",
-        answers: {
-            a: 'Return of the Mummy ',
-            b: 'Legends ',
-            c: 'Mission Impossible ',
-            d: 'Rock of Ages ',
-        },
-        correctAnswer: 'a',
-    },
-    {
-        question:  "In Top Gun, who was Maverick's Radar Intercept Officer and best friend?",        answers: {
-            a: 'Viper',
-            b: 'Ice Man ',
-            c: 'Goose ',
-            d: 'Cougar',
-        },
-        correctAnswer: 'c'
-    },
-    {
-        question:  "Which of the following actors did not play God on the big screen?",
-        answers: {
-            a: 'Rob Zomie ',
-            b: 'Morgan Freeman ',
-            c: 'Alanis Morissette ',
-            d: 'John Travolta ',
-        },
-        correctAnswer: 'd',
-    },
-    
+    question ("Which film ranked first in domestic box office for 2019 ",['Captain Marvel', 'Star WArs: Episode IX - The Rise of Skywalker','Toy Story 4','Avengers: Endgame'],'Avengers: Endgame'),
+    question ("Which Actress played Danielle De Barbarac in the film Ever After",['Maggie Gyllenhall','Drew Barrymore','Scarlett Johansson','Jennifer Connelly'],'Drew Barrymore'),
+    question ("What actor played the Joker in 2019", ['Heath Ledger ','Joaquin Phoenix ','Jared Leto ','Jack Nicholson '], 'Joaquin Phoenix'),
+    question ("What sequel made over 200,000,000 in 2019", ['Jumanji: The Next Level ','It Chapeter Two ','The Secret Life of Pets 2 ','Zombieland: Double Tap'],'It Chapter Two'),
+    question ("Which actor was the voice of Buzz Lightyear in Disney Pixar's Toy Story Franchise?",['Timmothy Hutton', 'Tom Hardy','Tim Allen','Tom Hanks',], 'Tim Allen'),
+    question ("Which Actor did not star in 1994's Pulp Fiction",['Bruce Willis ','Christopher Walken','Harvey Keitel ','Patricia Arquette'], 'Patricia Arquette'),
+    question ("What did Hannibal Lecter eat with some fava beans and a nice Chianti?",['Brain ','Lung ', 'Liver ','Kidney '],'Liver'),
+    question ("Which of the following did Tom Cruise NOT star in?",['Return of the Mummy ', 'Legends ','Mission Impossible ', 'Rock of Ages'],'Return of the Mummy'),
+    question ("In Top Gun, who was Maverick's Radar Intercept Officer and best friend?",['Viper', 'Goose ', 'Ice Man ',  'Cougar'],'Goose'),
+    question ("Which of the following actors did not play God on the big screen?", ['Rob Zomie ','Morgan Freeman ', 'Alanis Morissette ', 'John Travolta '], 'John Travolta')
 ]
