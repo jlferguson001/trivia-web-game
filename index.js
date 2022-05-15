749fd8b300c66df415f09ab2ae886fe11c895382
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-05-15 09:22:03 -0500

    updated quiz app made 1 player

--- index.js
+++ index.js
@@ -1,15 +1,188 @@
 document.body.style.backgroundImage = "url('./assets/krzyztopor-castle-1232338.jpg') ";
- 
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
 
-var myQuestions = [
-    question ("Which film ranked first in domestic box office for 2019 ",['Captain Marvel', 'Star WArs: Episode IX - The Rise of Skywalker','Toy Story 4','Avengers: Endgame'],'Avengers: Endgame'),
-    question ("Which Actress played Danielle De Barbarac in the film Ever After",['Maggie Gyllenhall','Drew Barrymore','Scarlett Johansson','Jennifer Connelly'],'Drew Barrymore'),
-    question ("What actor played the Joker in 2019", ['Heath Ledger ','Joaquin Phoenix ','Jared Leto ','Jack Nicholson '], 'Joaquin Phoenix'),
-    question ("What sequel made over 200,000,000 in 2019", ['Jumanji: The Next Level ','It Chapeter Two ','The Secret Life of Pets 2 ','Zombieland: Double Tap'],'It Chapter Two'),
-    question ("Which actor was the voice of Buzz Lightyear in Disney Pixar's Toy Story Franchise?",['Timmothy Hutton', 'Tom Hardy','Tim Allen','Tom Hanks',], 'Tim Allen'),
-    question ("Which Actor did not star in 1994's Pulp Fiction",['Bruce Willis ','Christopher Walken','Harvey Keitel ','Patricia Arquette'], 'Patricia Arquette'),
-    question ("What did Hannibal Lecter eat with some fava beans and a nice Chianti?",['Brain ','Lung ', 'Liver ','Kidney '],'Liver'),
-    question ("Which of the following did Tom Cruise NOT star in?",['Return of the Mummy ', 'Legends ','Mission Impossible ', 'Rock of Ages'],'Return of the Mummy'),
-    question ("In Top Gun, who was Maverick's Radar Intercept Officer and best friend?",['Viper', 'Goose ', 'Ice Man ',  'Cougar'],'Goose'),
-    question ("Which of the following actors did not play God on the big screen?", ['Rob Zomie ','Morgan Freeman ', 'Alanis Morissette ', 'John Travolta '], 'John Travolta')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+// Start game
+function startGame(){
+    console.log('started')
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() =>Math.random() -.5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+    
+}
+//next question
+
+function setNextQuestion(){
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+function showQuestion(question){
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if(answer.correct){
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+
+//select answer
+function resetState(){
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while(answerButtonsElement.firstChild){
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+ 
+function selectAnswer(e) {
+    const selectedButton =e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if(shuffledQuestions.length > currentQuestionIndex +1) {
+        nextButton.classList.remove('hide')
+    }else {
+        startButton.innerText = 'restart'
+        startButton.classList.remove('hide')
+    }
+
+    nextButton.classList.remove('hide')
+
+    function setStatusClass(element, correct) {
+        clearStatusClass(element)
+        if (correct) {
+            element.classList.add('correct')
+        }else {
+            element.classList.add('wrong')
+        }
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+const questions = [
+    {
+        question: "Which film ranked first in domestic box office for 2019?",
+        answers: [
+            {text:'Captain Marvel', correct: false},
+            {text: 'Star WArs: Episode IX - The Rise of Skywalker', correct: false},
+            {text:'Toy Story 4', correct: false},
+            {text:'Avengers: Endgame', correct: true},
+        ]
+    },
+        
+    {
+        question: "Which Actress played Danielle De Barbarac in the film Ever After?",
+        answers: [
+            {text:'Maggie Gyllenhall', correct: false},
+            {text: 'Drew Barrymore', correct: true},
+            {text: 'Scarlett Johansson', correct: false},
+            {text: 'Jennifer Connelly', correct: false},
+        ]
+    },
+    
+    {
+        question: "What actor played the Joker in 2019?", 
+        answers: [
+            {text:'Heath Ledger ', correct: false},
+            {text: 'Joaquin Phoenix', correct: true},
+            {text: 'Jared Leto ', correct: false},
+            {text: 'Jack Nicholson', correct: false},
+         ]
+    }, 
+
+    {
+        question: "What sequel made over 200,000,000 in 2019?", 
+        answers: [
+            {text: 'Jumanji: The Next Level', correct: false},
+            {text:'It Chapeter Two ', correct: true},
+            {text:'The Secret Life of Pets 2', correct: false},
+            {text: 'Zombieland: Double Tap', correct: false},
+        ]
+    },
+
+    {
+        question: "Which actor was the voice of Buzz Lightyear in Disney Pixar's Toy Story Franchise?",
+        answers: [
+            {text: 'Timmothy Hutton', correct: false},
+            {text: 'Tom Hardy', correct: false},
+            {text: 'Tim Allen', correct: true},
+            {text: 'Tom Hanks', correct: false},
+        ]
+    },
+
+    {
+        question:"Which Actor did not star in 1994's Pulp Fiction?",
+        answers: [
+            {text:'Bruce Willis ', correct: false},
+            {text:'Christopher Walken', correct: false},
+            {text: 'Harvey Keitel', correct: false},
+            {text: 'Patricia Arquette', correct: true},
+        ]
+    },
+
+    {
+        question:"What did Hannibal Lecter eat with some fava beans and a nice Chianti?",
+        answers: [
+            {text: 'Brain ', correct: false},
+            {text: 'Lung ', correct: false},
+            {text: 'Kindney', correct: false},
+            {text: 'Liver ', correct: true},
+        ]
+    },
+
+    {
+        question: "Which of the following did Tom Cruise NOT star in?",
+        answers: [
+            {text:'Return of the Mummy ', correct: true},
+            {text: 'Legends ', correct: false},
+            {text: 'Mission Impossible ', correct: false},
+            {text: 'Rock of Ages', correct: false},
+        ]
+    },
+
+    {
+        question: "In Top Gun, who was Maverick's Radar Intercept Officer and best friend?",
+        answers: [
+            {text: 'Viper', correct: false},
+            {text: 'Goose', correct: true},
+            {text: 'Ice Man', correct: false},
+            {text: 'Cougar', correct: false},
+        ]
+    },
+
+    {
+        question:"Which of the following actors did not play God on the big screen?", 
+        answers :[
+            {text: 'Rob Zomie', correct: false},
+            {text:'Morgan Freeman', correct: false},
+            {text: 'Alanis Morissette', correct: false},
+            {text: 'John Travolta', correct: true},
+        ],
+    },
+        
 ]
